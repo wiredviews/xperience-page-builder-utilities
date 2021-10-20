6a4364ee69661d5de60f2a8a954a9d7fde13306e
Sean Wright <sean@wiredviews.com>
2021-10-20 00:49:55 -0400

    docs(PageBuilderUtilities): service collection extension

--- src/XperienceCommunity.PageBuilderUtilities/PageBuilderUtilitiesServiceCollectionExtensions.cs
+++ src/XperienceCommunity.PageBuilderUtilities/PageBuilderUtilitiesServiceCollectionExtensions.cs
@@ -4,6 +4,11 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class PageBuilderContextServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds the Page Builder Context, which can be accessed by <see cref="IPageBuilderContext" />
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
         public static IServiceCollection AddPageBuilderContext(this IServiceCollection services) =>
             services.AddSingleton<IPageBuilderContext, XperiencePageBuilderContext>();
     }
